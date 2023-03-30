812b2cf14eab896d1337bf8b4be26e5f03875cef
Matthew Leibowitz <mattleibow@live.com>
2023-03-30 23:30:58 +0200

    Add it all

--- build.ps1
+++ build.ps1
@@ -1,5 +1,9 @@
 # remove things from the cache
-Remove-Item -recurse -force packages\microsoft.net.sdk.maui
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue packages\microsoft.net.sdk.maui\ | Out-Null
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue source\bin\ | Out-Null
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue source\obj\ | Out-Null
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue sample\bin\ | Out-Null
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue sample\obj\ | Out-Null
 
 # pack the new SDK
 dotnet pack source
