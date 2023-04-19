489de72212a408c6312685afec8128002055bf83
Matthew Leibowitz <mattleibow@live.com>
2023-04-19 15:35:12 +0700

    Use the feed now

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 # remove things from the cache
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue packages\microsoft.net.sdk.maui\ | Out-Null
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue packages\microsoft.maui.* | Out-Null
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue source\bin\ | Out-Null
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue source\obj\ | Out-Null
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue sample\bin\ | Out-Null
@@ -9,4 +10,5 @@ Remove-Item -Recurse -Force -ErrorAction SilentlyContinue sample\obj\ | Out-Null
 dotnet pack source
 
 # build the app
-dotnet build sample /bl
+dotnet restore sample /bl
+# dotnet build sample /bl
